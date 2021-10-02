251bb642776d58cc84eb88db61120924d5bc7af8
MRIDHUMRIDHULA <91274190+MRIDHUMRIDHULA@users.noreply.github.com>
2021-10-02 18:37:52 +0530

    Create Write.py

---
+++ Write.py
@@ -0,0 +1,39 @@
+import csv 
+
+def write_into_csv(info list):
+
+    with open('student_into.csv','w+',newline=' ') as csv_file:
+
+        writer = csv.writer(csv_file)
+
+        writer.writerow(info.list)
+
+        
+condition=true
+
+
+        
+
+while(condition):
+
+    student_info=input("Enter student information in following format (name  age  contact_number  E_mail_ID): ")
+
+    print("Entered information" + student_info)
+
+    
+
+    student_info_list = student_info.split(' ')
+
+    print("Entered split up information is : " +str(student_info_list))
+
+   
+
+   write_into_csv(student_info_list)
+
+   
+
+   condition check=input("Enter (yes/no) if you want to enter information for another students: ")
+   if condition_check == "yes":
+    condition = true
+   if condition_check == "no":
+    condition = false
